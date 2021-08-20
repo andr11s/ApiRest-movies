2a6dce4b93eda32663e9d2d6cc037321c12d91c3
andr11s <alvisandres1@gmail.com>
2021-08-20 09:49:15 -0500

    feact: agregue configuracion para mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config/config.module';
+import { ConfigService } from './config/config/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configservice: ConfigService) =>
+        configservice.getMongoConfig(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config/config.service.ts
@@ -0,0 +1,39 @@
+import * as dotenv from 'dotenv';
+
+export class ConfigService {
+  private readonly envConfig: Record<string, string>;
+
+  constructor() {
+    const result = dotenv.config();
+    result.error
+      ? (this.envConfig = process.env)
+      : (this.envConfig = result.parsed);
+  }
+
+  public get(key: string): string {
+    return this.envConfig[key];
+  }
+
+  public async getPortConfig() {
+    return this.get('PORT');
+  }
+
+  public async getMongoConfig() {
+    return {
+      uri:
+        'mongodb+srv://' +
+        this.get('USER') +
+        ':' +
+        this.get('PASSWORD') +
+        '@' +
+        this.get('HOST') +
+        '/' +
+        this.get('DATABASE') +
+        '?retryWrites=true&w=majority',
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    };
+  }
+}
